9a45e148f16bc0bebd451863ae43d0a40b1ac47f
Polina Yausheva <yausheva321@gmail.com>
2023-12-07 13:47:38 +0300

    пум пум

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -6,7 +6,7 @@ console.log('Welcome to the Brain Games!')
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`)
 
-console.log('Find the greatest common divisor of given numbers.')
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
 
 let n = 0
 let m = 0
