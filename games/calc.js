fb4cb6b545b70c585c116f26e6da1ecaf18d9200
Polina Yausheva <yausheva321@gmail.com>
2024-05-24 14:02:03 +0300

    правки правок

--- games/calc.js
+++ games/calc.js
@@ -1,33 +1,30 @@
 import gameStructure from '../src/index.js';
 
-const getEquation = (sign, x, y, i) => {
-  let num1 = x;
-  let num2 = y;
-  let result = 0;
-  if (sign[i] === '-') {
-    if (num2 > num1) {
-      const num = num1;
-      num1 = num2;
-      num2 = num;
-    }
-    result = num1 - num2;
-  } else if (sign[i] === '+') {
-    result = num1 + num2;
-  } else if (sign[i] === '*') {
-    result = num1 * num2;
-  }
-  return result;
-};
-
 const calcConditions = () => {
   const symbol = ['+', '-', '*']; // возможные математические операторы
   const i = Math.floor(Math.random() * 3); // индекс случайного оператора
-  const num1 = Math.floor(Math.random() * 100); // первое число
-  const num2 = Math.floor(Math.random() * 100); // второе число
+  let num1 = Math.floor(Math.random() * 100); // первое число
+  let num2 = Math.floor(Math.random() * 100); // второе число
 
   let result = 0;
 
-  result = getEquation(symbol, num1, num2, i);
+  const getEquation = (sign) => {
+    if (sign[i] === '-') {
+      if (num2 > num1) {
+        const num = num1;
+        num1 = num2;
+        num2 = num;
+      }
+      result = num1 - num2;
+    } else if (sign[i] === '+') {
+      result = num1 + num2;
+    } else if (sign[i] === '*') {
+      result = num1 * num2;
+    }
+    return result;
+  };
+
+  result = getEquation(symbol);
 
   const condition = `${num1} ${symbol[i]} ${num2}`;
 
