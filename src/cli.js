16ba6ea8c1fde2a3f350ee6b724437173edb796e
Polina Yausheva <yausheva321@gmail.com>
2024-06-05 13:46:32 +0300

    и ещё раз

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,8 @@ import readlineSync from 'readline-sync';
 const sayHello = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  return `Hello, ${name}!`;
+  console.log(`Hello, ${name}!`);
+  return name;
 };
 
 export default sayHello;
