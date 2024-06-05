e4fad98fae21e065060aa36d1886028d9df8f318
Polina Yausheva <yausheva321@gmail.com>
2024-06-05 13:53:44 +0300

    калькулятор

--- games/calc.js
+++ games/calc.js
@@ -1,15 +1,8 @@
 import gameStructure from '../src/index.js';
 
-const getEquationAnswer = (sign, x, y, i) => {
-  let num1 = x;
-  let num2 = y;
+const getEquationAnswer = (sign, num1, num2, i) => {
   let result = 0;
   if (sign[i] === '-') {
-    if (num2 > num1) {
-      const num = num1;
-      num1 = num2;
-      num2 = num;
-    }
     result = num1 - num2;
   } else if (sign[i] === '+') {
     result = num1 + num2;
